dbd8293f80f0edfff74c5d1853381dfa8da63cfc
Luizaescarban <luizaescarbandasil@gmail.com>
2025-08-30 17:32:01 -0300

    FEAT: inclusão de categoria no produto

--- core/models/produto.py
+++ core/models/produto.py
@@ -1,10 +1,15 @@
 from django.db import models
 
+from .categoria import Categoria
+
 
 class Produto(models.Model):
     nome = models.CharField(max_length=100)
     descricao = models.TextField()
     preco = models.DecimalField(max_digits=10, decimal_places=2)
+    categoria = models.ForeignKey(
+        Categoria, on_delete=models.PROTECT, related_name='produto', null=True, blank=True
+    )
 
     def __str__(self):
-        return f'({self.id}) {self.titulo} ({self.quantidade})'
+        return f'({self.id}) {self.nome} ({self.categoria})'
