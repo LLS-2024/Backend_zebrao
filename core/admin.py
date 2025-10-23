79b82bad1f4494bcb70284d37d8186ac624c99c1
Luizaescarban <luizaescarbandasil@gmail.com>
2025-10-23 11:10:18 -0300

    feat: uso de TabularInline e StackedInline no Admin para Itens da Compra

--- core/admin.py
+++ core/admin.py
@@ -7,7 +7,7 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
 
 from core import models
-from core.models import Compra
+from core.models import Compra, ItensCompra
 
 
 class UserAdmin(BaseUserAdmin):
@@ -72,11 +72,19 @@ class ProdutoAdmin(admin.ModelAdmin):
     list_per_page = 25
 
 
+class ItensCompraInline(admin.TabularInline):
+    model = ItensCompra
+    extra = 1  # Quantidade de itens adicionais
+
+
 @admin.register(Compra)
 class CompraAdmin(admin.ModelAdmin):
     list_display = ('usuario', 'status')
+    search_fields = ('usuario', 'status')
+    list_filter = ('usuario', 'status')
     ordering = ('usuario', 'status')
     list_per_page = 10
+    inlines = [ItensCompraInline]
 
 
 admin.site.register(models.User, UserAdmin)
