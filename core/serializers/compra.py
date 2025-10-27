ec770c65f3bb040eba26020126fe8e185f30ec0f
Luizaescarban <luizaescarbandasil@gmail.com>
2025-10-27 16:47:09 -0300

    feat: criação de um endpoint para criar novas compras

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -1,14 +1,15 @@
-
-from rest_framework.serializers import CharField, ModelSerializer, SerializerMethodField
+from django.db import transaction
+from rest_framework import serializers
 
 from core.models import Compra, ItensCompra
 
 
-class ItensCompraSerializer(ModelSerializer):
-    total = SerializerMethodField()
+class ItensCompraSerializer(serializers.ModelSerializer):
+    total = serializers.SerializerMethodField()
 
     def get_total(self, instance):
-        return instance.livro.preco * instance.quantidade
+        # usar 'produto' (não 'livro')
+        return instance.produto.preco * instance.quantidade
 
     class Meta:
         model = ItensCompra
@@ -16,10 +17,35 @@ class ItensCompraSerializer(ModelSerializer):
         depth = 1
 
 
-class CompraSerializer(ModelSerializer):
+class CompraSerializer(serializers.ModelSerializer):
+    itens = ItensCompraSerializer(many=True, read_only=True)
+    usuario = serializers.CharField(source='usuario.email', read_only=True)
+    status = serializers.CharField(source='get_status_display', read_only=True)
+
     class Meta:
-        itens = ItensCompraSerializer(many=True, read_only=True)
-        usuario = CharField(source='usuario.email', read_only=True)
-        status = CharField(source='get_status_display', read_only=True)
         model = Compra
         fields = ('id', 'usuario', 'status', 'total', 'itens')
+
+
+class ItensCompraCreateUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ItensCompra
+        fields = ('produto', 'quantidade')
+
+
+class CompraCreateUpdateSerializer(serializers.ModelSerializer):
+    itens = ItensCompraCreateUpdateSerializer(many=True)
+
+    class Meta:
+        model = Compra
+        fields = ('usuario', 'itens')
+
+    def create(self, validated_data):
+        itens_data = validated_data.pop('itens')
+        # opcional: definir usuário pelo request (se quiser evitar enviar usuario no body)
+        with transaction.atomic():
+            compra = Compra.objects.create(**validated_data)
+            for item_data in itens_data:
+                ItensCompra.objects.create(compra=compra, **item_data)
+            compra.save()
+        return compra
